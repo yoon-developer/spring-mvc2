e7bf25813bd42685ffc20c26cabe83676a395f13
Yoon <yoondev93@gmail.com>
2022-09-05 09:35:56 +0900

    feature: @ExceptionHandle

---
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,44 @@
+package hello.exception.api;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api2")
+public class ApiExceptionV2Controller {
+
+  @GetMapping("/members/{id}")
+  public MemberDto getMember(@PathVariable("id") String id) {
+    if (id.equals("ex")) {
+      throw new RuntimeException("잘못된 사용자");
+    }
+    if (id.equals("bad")) {
+      throw new IllegalArgumentException("잘못된 입력 값");
+    }
+    if (id.equals("user-ex")) {
+      throw new UserException("사용자 오류");
+    }
+    return new MemberDto(id, "hello " + id);
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberDto {
+
+    private String memberId;
+
+    private String name;
+  }
+}
