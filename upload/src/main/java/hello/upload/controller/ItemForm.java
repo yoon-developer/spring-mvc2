1fcf9a77311684c04ec9005872f2601f5101b4dd
Yoon <yoondev93@gmail.com>
2022-10-02 02:12:34 +0900

    feature: 예제로 구현하는 파일 업로드, 다운로드

---
+++ upload/src/main/java/hello/upload/controller/ItemForm.java
@@ -0,0 +1,18 @@
+package hello.upload.controller;
+
+import java.util.List;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class ItemForm {
+
+  private Long itemId;
+
+  private String itemName;
+
+  private List<MultipartFile> imageFiles;
+
+  private MultipartFile attachFile;
+
+}
